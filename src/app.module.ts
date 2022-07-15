697b64897da9a0d2a7a6c57170915777422245ec
xdth <info@dthlabs.com>
2022-07-14 23:30:20 -0400

    Adding TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      type: 'sqlite',
+      database: '../db',
+      entities: [User],
       synchronize: true,
     }),
     UsersModule
